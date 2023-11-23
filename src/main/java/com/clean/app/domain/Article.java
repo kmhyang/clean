ceeb1b383732d3ff7cad56e6115b9cf7344e59e3
k-Hailey <mhkim@hu1.com>
2023-11-23 15:02:59 +0900

    domain - Article, ArticleComment

---
+++ src/main/java/com/clean/app/domain/Article.java
@@ -0,0 +1,16 @@
+package com.clean.app.domain;
+
+import java.time.LocalDateTime;
+
+public class Article {
+    private Long id;
+    private String tile;
+    private String content;
+    private String hashtag;
+
+    private LocalDateTime createdAt;
+    private String createdBy;
+     private LocalDateTime modifiedAt;
+    private String modifiedBy;
+
+}
