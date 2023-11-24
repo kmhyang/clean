50607f12858b2be7eb6c129c13edb1b2d7f5806e
k-Hailey <mhkim@hu1.com>
2023-11-24 15:56:31 +0900

    api구현 / view test

---
+++ src/test/java/com/clean/app/controller/ArticleControllerTest.java
@@ -0,0 +1,26 @@
+package com.clean.app.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+@DisplayName("View 컨트롤러 - 게시글")
+//@Import({TestSecurityConfig.class, FormDataEncoder.class})
+@WebMvcTest(ArticleController.class)
+class ArticleControllerTest {
+
+    private final MockMvc mvc;
+
+    public ArticleControllerTest(@Autowired MockMvc mvc) {
+        this.mvc = mvc;
+    }
+
+    @DisplayName("[view][GET] 게시글 리스트 (게시판) 페이지 정상 호출")
+    @Test
+    void givenNothing_whenRequestArticlesView_thenReturnArticlesView() {
+    }
+}
