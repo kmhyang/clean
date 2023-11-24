cd015d4a8aaa7ad5e14d3cf109647c655a475d23
k-Hailey <mhkim@hu1.com>
2023-11-24 15:28:15 +0900

    Data Rest 적용 및 테스트

--- src/main/java/com/clean/app/repository/ArticleCommentRepository.java
+++ src/main/java/com/clean/app/repository/ArticleCommentRepository.java
@@ -2,6 +2,8 @@ package com.clean.app.repository;
 
 import com.clean.app.domain.ArticleComment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
 }
--- src/main/java/com/clean/app/repository/ArticleRepository.java
+++ src/main/java/com/clean/app/repository/ArticleRepository.java
@@ -2,6 +2,8 @@ package com.clean.app.repository;
 
 import com.clean.app.domain.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 }
